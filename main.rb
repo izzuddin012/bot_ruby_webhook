e8192355a6bbfbcd8b43be385d3997ac2d83dbff
Muhammad Izzuddin <izzuddin@bukalapak.com>
2018-05-28 14:52:08 +0700

    auto remove message for some comment

--- main.rb
+++ main.rb
@@ -20,16 +20,16 @@ class Main < Sinatra::Base
     if update['message']
       message = update['message']
       puts message.to_s
-      reply = do_something_with_text(message['text'], message['from']['username'])
-      settings.bot.api.send_message(chat_id: message['chat']['id'], text: reply, reply_to_message_id: message['message_id'])
+      reply = do_something_with_text(message['text'], message['message_id'], message['chat']['id'])
+      settings.bot.api.send_message(chat_id: message['chat']['id'], text: reply)
     end
     200
     # content_type :json
     # reply.to_json
   end
 
-  def do_something_with_text(text, username)
-    reply = text
+  def do_something_with_text(text, message_id, chat_id)
+    reply = ''
     splitted_text = text.split
     command = splitted_text[0]
 
@@ -37,11 +37,16 @@ class Main < Sinatra::Base
       reply = fetch_todays_event()
     elsif command == '/remote' || command == '/leave'
       reply = create_event(text, command)
+      remove_message(message_id, chat_id)
     elsif command == '/help'
       reply = "/remote <telegram username> <start_date> <end_date> - Create remote event\n" +
               "/leave <telegram username> <start_date> <end_date> - Create leave event\n"
     end
-    reply# return
+    reply
+  end
+
+  def remove_message(message_id, chat_id)
+    settings.bot.api.delete_message(chat_id: chat_id, message_id: message_id)
   end
 
   def fetch_todays_event()
@@ -95,8 +100,6 @@ class Main < Sinatra::Base
           headers: {'Teamup-Token': "#{ENV['TEAMUP_TOKEN']}", 'Content-type': 'application/json'}
         )
         reply = "#{response.code == 201 ? 'Event berhasil dibuat' : 'Event gagal dibuat'}"
-
-        # reply = post_params
 
       rescue ArgumentError  
         reply = 'Format tanggal mulai atau selesai salah'
